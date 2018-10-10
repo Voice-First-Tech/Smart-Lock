158205eb49a92bf103608f1d7145fe6fb7499b7b
Shaffan Mustafa <shaffan1996@gmail.com>
2018-10-09 22:27:44 -0400

    made a file with some examples from the winston documentation detailing how to transmit events and log unhandled exceptions

--- use_winston.js
+++ use_winston.js
@@ -24,3 +24,34 @@ var winston = require('winston');
     logger.info('logging before I close');
     logger.close(); // this closes the underlying connection in the Papertrail transport
 });
+
+///
+//
+// Exception handling with winston
+//
+//
+
+const { createLogger, transports } = require('winston');
+
+// Enable exception handling when you create your logger.
+const logger = createLogger({
+  transports: [
+    new transports.File({ filename: 'combined.log' })
+  ],
+  exceptionHandlers: [
+    new transports.File({ filename: 'exceptions.log' })
+  ]
+});
+
+// Or enable it later on by adding a transport or using `.exceptions.handle`
+const logger = createLogger({
+  transports: [
+    new transports.File({ filename: 'combined.log' })
+  ]
+});
+
+// Call exceptions.handle with a transport to handle exceptions
+logger.exceptions.handle(
+  new transports.File({ filename: 'exceptions.log' })
+);
+
