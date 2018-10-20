6af2858e9a47373ee32e0b0657aee36bd7a1e02d
Shaffan Mustafa <shaffan1996@gmail.com>
2018-10-19 21:58:37 -0400

    currently on part 2 of the smart lock tutorial

---
+++ vipermark7/app/app.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// =================================================================================
+// App Configuration
+// =================================================================================
+
+const {App} = require('jovo-framework');
+
+const config = {
+    // logging: true,
+};
+
+const app = new App(config);
+
+
+// =================================================================================
+// App Logic
+// =================================================================================
+
+app.setHandler({
+    'LAUNCH': function() {
+	let response = this.speechBuilder()
+	    .addText("I can help you control Smart Locks. Say lock the front door or unlock the front door")
+	let reprompt = this.speechBuilder()
+	    .addText("Say lock the front door or unlock the front door")
+        this.followUpState('FirstState')
+        .ask("I can help you control Smart Locks. Say lock the front door or unlock the front door",
+            "Say lock the front door or unlock the front door");
+    },
+
+
+    'FirstState': {
+        'MyNameIsIntent': function(name) {
+            this.toStatelessIntent('MyNameIsIntent', name);
+        },
+
+        // // Test fails if this is commented out
+        // 'Unhandled': function(name) {
+        //     this.ask('What\'s your name?');
+        // },
+    },
+
+    'MyNameIsIntent': function(name) {
+        this.tell('Hey ' + name.value + ', nice to meet you!');
+    },
+
+});
+
+module.exports.app = app;
---
+++ vipermark7/test/Launch-Test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+const getPlatformRequestBuilder = require('jovo-framework').util.getPlatformRequestBuilder;
+const {send} = require('jovo-framework').TestSuite;
+
+
+for (let rb of getPlatformRequestBuilder('AlexaSkill', 'GoogleActionDialogFlowV2')) {
+    describe(`PLATFORM: ${rb.type()}`, () => {
+        // Test isolated intents, "deep invocations"
+        describe('LAUNCH-INTENTS', () => {
+                // Invocation: "open my test app"
+                it('should return a welcome message and ask for the name at "LAUNCH"', () => {
+                    return send(rb.launch())
+                        .then((res) => {
+                            const matchesResponse = res.isAsk(launchResponse, launchReprompt);
+                            expect(matchesResponse).to.equal(true);
+                        });
+                });
+            });
+        });
+};
+
+let launchResponse = "I can help you control Smart Locks. Say lock the front door or unlock the front door";
+let launchReprompt = "Say lock the front door or unlock the front door";
