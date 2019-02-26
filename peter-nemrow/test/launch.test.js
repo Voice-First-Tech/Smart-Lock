2b07c87558daba88802e3be1c349c7159abcbd6e
pnemrow3 <pnemrow@gatech.edu>
2019-02-26 08:52:55 -0700

    I modified launch tests to welcome customers to their smart lock and guide them towards a proper response

--- peter-nemrow/test/launch.test.js
+++ peter-nemrow/test/launch.test.js
@@ -8,15 +8,18 @@ for (const p of [new Alexa(), new GoogleAssistant()]) {
     const testSuite = p.makeTestSuite();
 
     describe(`PLATFORM: ${p.constructor.name} INTENTS` , () => {
-        test('should return a welcome message and ask for the name at "LAUNCH"', async () => {
+        test('should return a welcome message and ask for door instruction at "LAUNCH"', async () => {
             const conversation = testSuite.conversation();
 
             const launchRequest = await testSuite.requestBuilder.launch();
             const responseLaunchRequest = await conversation.send(launchRequest);
             expect(
-                responseLaunchRequest.isAsk('Hello World! What\'s your name?', 'Please tell me your name.')
+                responseLaunchRequest.isAsk(expectedLaunchPrompt, expectedLuanchReprompt)
             ).toBe(true);
 
         });
     });
 }
+
+let expectedLaunchPrompt = 'Welcome to your smart lock, Would you like to lock your door? Check the status of your door? Or unlock your door?'
+let expectedLuanchReprompt = 'Please say lock door, status of my door, or unlock to control your door.'
